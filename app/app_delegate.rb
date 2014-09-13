095fdef165ffde30e3be52977d538f5aa234e1fd
Erik Nilsen <enilsen16@live.com>
2014-09-13 11:01:09 -0700

    add inital view and tests

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    @window.rootViewController = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController.view.backgroundColor = UIColor.yellowColor
+
     true
   end
 end
